11c1dcce6c7915abf004de94da909f0c60369ee1
hiraoku.shinichi <s.hiraoku@gmail.com>
2025-04-22 10:02:07 +0900

    add mcp server template.

---
+++ mcp-server/src/index.ts
@@ -0,0 +1,7 @@
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StreamableHTTPServerTransport } from "@modelcontextprotocol/sdk/server/streamableHttp.js";
+import { z } from "zod";
+import express from "express";
+
+const app = express();
+app.use(express.json());
